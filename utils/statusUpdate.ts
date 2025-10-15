6721c09ef46cce79ab274c7a77bf0230601dd3e2
Tekina <nm0321@yandex.ru>
2025-10-15 22:23:14 +0300

    update status x2

--- utils/statusUpdate.ts
+++ utils/statusUpdate.ts
@@ -6,13 +6,13 @@ import {ActivityType} from "discord.js"
 export default async () => {
     const arr = await rcon()
     client.user?.setPresence({
-            activities: [{ name: arr?.length > 0 ? `На сервере: ${arr.length} / 40 игроков` : 'Нет данных от сервера', type: ActivityType.Watching }],
+            activities: [{ name: arr?.length > 0 ? `на онлайн: ${arr.length} / 40` : 'Нет данных от сервера', type: ActivityType.Watching }],
             status: "online",
         })
     setInterval(async () => {
         const arr = await rcon()
         client.user?.setPresence({
-            activities: [{ name: arr?.length > 0 ? `На сервере: ${arr.length} / 40 игроков` : 'Нет данных от сервера', type: ActivityType.Watching }],
+            activities: [{ name: arr?.length > 0 ? `на онлайн: ${arr.length} / 40` : 'Нет данных от сервера', type: ActivityType.Watching }],
             status: "online",
         })
     }, 60000);
